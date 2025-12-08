1c82c5aa9e5087991d183ae61fb9a84ededdbf5a
mhmdtaqi <mhmdtaqi16@gmail.com>
2025-12-08 11:25:00 +0700

    fix url fe

--- src/app.ts
+++ src/app.ts
@@ -24,7 +24,11 @@ const PORT = process.env.PORT || 5000;
 app.use(helmet());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    origin: [ 
+      "http://localhost:3000",
+      "http://bmn-faste.vercel.app",  
+      process.env.FRONTEND_URL || ""
+    ],
     credentials: true,
   })
 );
