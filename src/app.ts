7cf02a2b35bf576ded3208d312555bc32c17c676
mhmdtaqi <mhmdtaqi16@gmail.com>
2025-12-08 14:07:57 +0700

    fix

--- src/app.ts
+++ src/app.ts
@@ -29,6 +29,24 @@ const allowedOrigins = [
   "https://bmn-faste.vercel.app",
 ];
 
+// Handle preflight OPTIONS requests
+app.options("*", (req, res) => {
+  const origin = req.headers.origin;
+  if (!origin || allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin || "*");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, OPTIONS"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Authorization"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+  }
+  res.sendStatus(200);
+});
+
 app.use(
   cors({
     origin: (origin, callback) => {
